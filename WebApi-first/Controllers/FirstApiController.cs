e2860d9ffaaf91c24a1b89361c7b99d1c4ba5ff7
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-08-31 16:36:25 +0300

    Make GET call between 2 Api

--- WebApi-first/Controllers/FirstApiController.cs
+++ WebApi-first/Controllers/FirstApiController.cs
@@ -4,44 +4,78 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApi_first.Controllers
 {
+    [ApiController]         //tell this class is API CONTROLLER and enable automatic validation os requests
     [Route("api/[controller]")]
     public class FirstApiController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        //Main Method
+        [HttpGet("Get")]
+        public IEnumerable<string> Get() //IEnumerable:   bec we need to return a list of strings.
         {
-            return new string[] { "value1", "value2" };
+            Console.WriteLine("Get into it"); //for debugging
+
+            //Create and write file log1.txt(for keeping log of how many times it called)
+
+            //string[] linesToAppend = new[] { "Go get called in Api1" };
+            //System.IO.File.AppendAllLines("log1.txt", linesToAppend);  OR
+
+            System.IO.File.AppendAllLines("log1.txt", new[] { "Go get called in Api1" });
+            //whenever the Get method is called, a new line will be added to the file.
+
+            System.Threading.Thread.Sleep(400); //400millisec delay/pause here in execution of next code
+
+
+            String[] greeting = new[] { "Moi from 1st Api" };
+            return greeting;
+            //or            return new[] { "Moi from 1st Api" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+
+
+        //Method to make Call
+        [HttpGet("CallApi2")]
+        public async Task<string> CallApi2() //this will eventually give a string after wait(task is ticket assigned)
         {
-            return "value";
+            //1. create instance/obj of HTTPClient class.It heandle HTTP Requests(call/Get/POST)
+            HttpClient client = new HttpClient();//For sending request
+
+            // Calling(Send a GET request and get the response)
+            //GetAsync: sends an asynchronous HTTP GET request to the specified URL
+            //await: wait for the reponse but let main method do next tasks
+
+            var response = await client.GetAsync("https://localhost:7256/api/SecondApi/RecieveFromApi");
+
+            if (response.IsSuccessStatusCode)
+            {
+                //response: obtained from above GET request. Content is content of the response
+                //ReadAsStringAsync(): Method of Content class that reads the content of the HTTP response as a string.
+
+                var responseData = await response.Content.ReadAsStringAsync();
+
+                return ("Successfully called API-2 " + responseData); //return content of response
+            }
+            else
+            {
+                return ("Error!" + response.StatusCode); //return code
+            }
+
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
+
+
+        //Method to handle the call request(from other Api) and provide response
+        [HttpGet("RecieveFromApi")]                       //Route for receieving request
+        public IActionResult RecieveFromApi2()
         {
+            var message = "Successfully received call from Api2 in Api1";
+            return Ok(message);
+
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
 
--- WebApi-second/Controllers/SecondApiController.cs
+++ WebApi-second/Controllers/SecondApiController.cs
@@ -4,43 +4,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApi_second.Controllers
 {
+    [ApiController]
     [Route("api/[controller]")]
     public class SecondApiController : Controller
     {
-        // GET: api/values
-        [HttpGet]
+
+        [HttpGet("Get")]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            Console.WriteLine("Go get in Api 2"); //fordebugging
+
+            System.IO.File.AppendAllLines("log2.txt", new[] { "Running the Api" });//for booklog
+
+            System.Threading.Thread.Sleep(400);
+
+
+
+            return new[] { "Moi from 2nd Api" };
+
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+
+
+
+
+
+        //Method to handle the call request(from other Api) and provide response
+        [HttpGet("RecieveFromApi")]                       //Route for receieving request
+        public IActionResult RecieveFromApi1()
         {
-            return "value";
+            var message = "Successfully received call from Api1 in Api2";
+            return Ok(message);
+
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
+        //Method to make Call
+        [HttpGet("CallApi1")]
+        public async Task<string> CallApi1()
         {
-        }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+            HttpClient client = new HttpClient();//For sending request
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var response = await client.GetAsync("https://localhost:7131/api/FirstApi/RecieveFromApi");
+
+            if (response.IsSuccessStatusCode)
+            {
+                //response: obtained from above GET request. Content is content of the response
+                //ReadAsStringAsync(): Method of Content class that reads the content of the HTTP response as a string.
+
+                var responseData = await response.Content.ReadAsStringAsync();
+
+                return ("Successfully called API-1 " + responseData); //return content of response
+            }
+            else
+            {
+                return ("Error!" + response.StatusCode); //return code
+            }
         }
     }
 }
